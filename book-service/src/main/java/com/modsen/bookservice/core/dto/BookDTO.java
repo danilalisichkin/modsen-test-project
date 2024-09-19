db2633d55471a634c4b291327e09d9699975fd16
danilalisichkin <danilafox3467346@gmail.com>
2024-09-19 19:09:58 +0300

    add book dto classes

---
+++ book-service/src/main/java/com/modsen/bookservice/core/dto/BookDTO.java
@@ -0,0 +1,21 @@
+package com.modsen.bookservice.core.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class BookDTO {
+
+    private Long id;
+
+    private String isbn;
+
+    private String title;
+
+    private String genre;
+
+    private String description;
+
+    private String author;
+}
