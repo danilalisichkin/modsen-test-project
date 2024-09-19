b77e8a536067ad5e9b0a9a2994d2b6e8b7f59577
danilalisichkin <danilafox3467346@gmail.com>
2024-09-19 19:05:02 +0300

    add book entity

---
+++ book-service/src/main/java/com/modsen/bookservice/entities/Book.java
@@ -0,0 +1,42 @@
+package com.modsen.bookservice.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 100, nullable = false)
+    private String genre;
+
+    @Column(length = 400, nullable = false)
+    private String description;
+
+    @Column(length = 100, nullable = false)
+    private String author;
+}
