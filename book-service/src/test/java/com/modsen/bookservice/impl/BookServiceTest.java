6d2d768bdf61815a1432df773fc7d296789d3a42
danilalisichkin <danilafox3467346@gmail.com>
2024-09-20 14:44:31 +0300

    change tests for BookService, make them actual

--- book-service/src/test/java/com/modsen/bookservice/impl/BookServiceTest.java
+++ book-service/src/test/java/com/modsen/bookservice/impl/BookServiceTest.java
@@ -8,6 +8,7 @@ import com.modsen.bookservice.core.dto.library.BookInventoryDTO;
 import com.modsen.bookservice.core.mappers.BookMapper;
 import com.modsen.bookservice.dao.repository.BookRepository;
 import com.modsen.bookservice.entities.Book;
+import com.modsen.bookservice.exceptions.DataUniquenessConflictException;
 import com.modsen.bookservice.exceptions.ResourceNotFoundException;
 import com.modsen.bookservice.services.impl.BookService;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,12 +17,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,6 +56,7 @@ public class BookServiceTest {
     void setUp() {
         book = Book.builder()
                 .id(1L)
+                .isbn("1234567890")
                 .title("Test title")
                 .author("Test author")
                 .description("Test description")
@@ -56,12 +64,14 @@ public class BookServiceTest {
                 .build();
         bookDTO = BookDTO.builder()
                 .id(1L)
+                .isbn("1234567890")
                 .title("Test title")
                 .author("Test author")
                 .description("Test description")
                 .genre("Test genre")
                 .build();
         bookAddingDTO = BookAddingDTO.builder()
+                .isbn("1234567890")
                 .title("Test title")
                 .author("Test author")
                 .description("Test description")
@@ -75,6 +85,17 @@ public class BookServiceTest {
         bookInventoryAddingDTO = BookInventoryAddingDTO.builder()
                 .bookId(1L)
                 .build();
+    }
+
+    @Test
+    void whenGetAllBooks_thenReturnBooks() {
+        when(bookRepository.findAll()).thenReturn(List.of(book, book));
+        when(bookMapper.entityToDto(book)).thenReturn(bookDTO);
+
+        List<BookDTO> allBooks = bookService.getAllBooks();
+
+        assertNotNull(allBooks);
+        assertEquals(List.of(bookDTO, bookDTO), allBooks);
     }
 
     @Test
@@ -97,10 +118,10 @@ public class BookServiceTest {
 
     @Test
     void whenGetExistingBookByIsbn_thenReturnBook() {
-        when(bookRepository.findByIsbn("123456789")).thenReturn(Optional.of(book));
+        when(bookRepository.findByIsbn("1234567890")).thenReturn(Optional.of(book));
         when(bookMapper.entityToDto(book)).thenReturn(bookDTO);
 
-        BookDTO returnedBook = bookService.getBookByISBN("123456789");
+        BookDTO returnedBook = bookService.getBookByISBN("1234567890");
 
         assertNotNull(returnedBook);
         assertEquals(bookDTO, returnedBook);
@@ -108,35 +129,67 @@ public class BookServiceTest {
 
     @Test
     void whenGetNotExistingBookByIsbn_thenThrowNotFound() {
-        when(bookRepository.findByIsbn("123456789")).thenReturn(Optional.empty());
+        when(bookRepository.findByIsbn("1234567890")).thenReturn(Optional.empty());
 
         assertThrows(ResourceNotFoundException.class,
-                () -> bookService.getBookByISBN("123456789"));
+                () -> bookService.getBookByISBN("1234567890"));
     }
 
     @Test
-    void whenSaveBook_thenReturnBook() {
-        when(bookRepository.save(book)).thenReturn(book);
+    void whenSaveValidBook_thenReturnBook() {
+        when(bookRepository.findByIsbn("1234567890")).thenReturn(Optional.empty());
         when(bookMapper.addingDtoToEntity(bookAddingDTO)).thenReturn(book);
+        when(bookRepository.save(book)).thenReturn(book);
         when(bookMapper.entityToInventoryAddingDto(book)).thenReturn(bookInventoryAddingDTO);
+        when(libraryClient.addBookInventory(bookInventoryAddingDTO)).thenReturn(ResponseEntity.ok(bookInventoryDTO));
         when(bookMapper.entityToDto(book)).thenReturn(bookDTO);
-        when(libraryClient.addBookInventory(bookInventoryAddingDTO)).thenReturn(null);
 
         BookDTO returnedBook = bookService.saveBook(bookAddingDTO);
 
         assertNotNull(returnedBook);
         assertEquals(bookDTO, returnedBook);
+        verify(libraryClient).addBookInventory(any());
     }
 
     @Test
-    void whenUpdateBook_thenReturnBook() {
-        when(bookRepository.save(book)).thenReturn(book);
+    void whenSaveNotValidBook_thenThrowDataConflict() {
+        when(bookRepository.findByIsbn("1234567890")).thenReturn(Optional.of(book));
+
+        assertThrows(DataUniquenessConflictException.class, () -> bookService.saveBook(bookAddingDTO));
+    }
+
+    @Test
+    void whenUpdateExistingBook_thenReturnBook() {
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
         when(bookMapper.dtoToEntity(bookDTO)).thenReturn(book);
+        when(bookRepository.save(book)).thenReturn(book);
         when(bookMapper.entityToDto(book)).thenReturn(bookDTO);
 
         BookDTO returnedBook = bookService.updateBook(bookDTO);
 
         assertNotNull(returnedBook);
         assertEquals(bookDTO, returnedBook);
+    }
+
+    @Test
+    void whenUpdateNotExistingBook_thenThrowNotFound() {
+        when(bookRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> bookService.updateBook(bookDTO));
+    }
+
+    @Test
+    void whenDeleteExistingBookById_thenSuccess() {
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+
+        assertDoesNotThrow(() -> bookService.deleteBookById(1L));
+        verify(bookRepository).deleteById(1L);
+    }
+
+    @Test
+    void whenDeleteNotExistingBook_thenThrowNotFound() {
+        when(bookRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> bookService.deleteBookById(1L));
     }
 }
