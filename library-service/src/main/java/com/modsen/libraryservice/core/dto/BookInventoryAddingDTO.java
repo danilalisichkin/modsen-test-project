b51adcf77330ab69703360ac1c65f2af54bb7b46
danilalisichkin <danilafox3467346@gmail.com>
2024-09-25 22:16:56 +0300

    fix test for BookInventoryMapper
    - remove loading of Spring Context
    - fix dto initial values
    - add equals() and hashcode() for dto

--- library-service/src/main/java/com/modsen/libraryservice/core/dto/BookInventoryAddingDTO.java
+++ library-service/src/main/java/com/modsen/libraryservice/core/dto/BookInventoryAddingDTO.java
@@ -3,6 +3,7 @@ package com.modsen.libraryservice.core.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import org.antlr.v4.runtime.misc.NotNull;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 @Schema(description = "Entry to add a book to library inventory")
 public class BookInventoryAddingDTO {
 
