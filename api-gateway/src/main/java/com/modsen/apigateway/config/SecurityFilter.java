69ba0143f9bdb9f369015b2fadbcffc6875de3e9
danilalisichkin <danilafox3467346@gmail.com>
2024-09-28 01:19:47 +0300

    add exception handlers, refactor global security filter

--- api-gateway/src/main/java/com/modsen/apigateway/config/SecurityFilter.java
+++ api-gateway/src/main/java/com/modsen/apigateway/config/SecurityFilter.java
@@ -1,52 +1,62 @@
 package com.modsen.apigateway.config;
 
 import com.modsen.apigateway.controllers.clients.AuthenticationClient;
-import com.modsen.apigateway.exceptions.ExceptionMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class SecurityFilter implements GlobalFilter {
+public class SecurityFilter implements GlobalFilter, Ordered {
     private final RouteValidator validator;
-
     private final AuthenticationClient authenticationClient;
 
     @Autowired
-    public SecurityFilter(RouteValidator validator, @Lazy AuthenticationClient authenticationClient) {
+    public SecurityFilter(RouteValidator validator, AuthenticationClient authenticationClient) {
         this.validator = validator;
         this.authenticationClient = authenticationClient;
     }
 
     @Override
-    public Mono<Void> filter(
-            ServerWebExchange exchange,
-            GatewayFilterChain chain) {
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         if (validator.isSecured.test(exchange.getRequest())) {
-
             if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "missing authorization header - authorization required");
+                return Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "missing authorization header - authorization required"));
             }
 
-            String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
             if (authHeader != null && authHeader.startsWith("Bearer ")) {
                 authHeader = authHeader.substring(7);
             }
 
-            ResponseEntity<Object> response = authenticationClient.validateToken(authHeader);
-            if (!response.getBody().equals("token valid") || !response.getStatusCode().equals(HttpStatus.OK)) {
-                ExceptionMessage responseMessage = (ExceptionMessage) response.getBody();
-                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, responseMessage.getMessage() + " - " + responseMessage.getCause());
-            }
+            return authenticationClient.validateToken(authHeader)
+                    .flatMap(response -> {
+                        if ("token valid".equals(response)) {
+                            return chain.filter(exchange);
+                        } else {
+                            return Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "invalid token - " + response));
+                        }
+                    })
+                    .onErrorResume(e -> {
+                        if (e instanceof WebClientResponseException) {
+                            return Mono.error(e);
+                        } else {
+                            return Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "un authorized access to application - authorization required", e));
+                        }
+                    });
         }
         return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
